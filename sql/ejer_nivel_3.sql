c3243614661d6c7b6071045fd6d8b93bfba82807
Valentin Perez G <tperezg@fi.uba.ar>
2026-04-25 21:51:47 -0300

    Iniciamos los ejercicios de nivel 3 de sql con los primeros 3
    Added SQL queries for various exercises related to bands, albums, and songs.

---
+++ sql/ejer_nivel_3.sql
@@ -0,0 +1,37 @@
+-- Ejercicio 1: El nombre de las bandas cuyos álbumes (todos) duran más de 50 minutos.
+
+select b.nombre from bandas b
+where not exists(
+  select 1 from albumes a
+  where a.banda_id = b.id
+  and a.duracion <= 50
+);
+
+-- Ejercicio 2: El nombre de las canciones (con el nombre de su álbum) que pertenecen a álbumes lanzados después del año 2000. Ordenarlas por nombre de álbum (de A a Z), y por cada álbum, por nombre de canción (de Z a A).
+
+select c.nombre as cancion, a.nombre as album from canciones c
+join albumes a on c.album_id = a.id
+where a.lanzamiento > 2000
+order by a.nombre asc, c.nombre desc;
+
+-- Ejercicio 3: El nombre de las bandas con menos de 5 integrantes y de género "Rock Alternativo", cuyas canciones duran 4 o más minutos (todas ellas).
+
+select b.nombre from bandas b
+where b.cant_integrantes < 5 
+and b.genero = 'Rock Alternativo'
+and not exists(
+  select 1 from canciones c
+  where c.banda_id = b.id
+  and c.duracion < 4
+);
+
+-- Ejercicio 4: El concierto con más bandas participantes, mostrando el nombre del concierto y la cantidad de bandas que participaron.
+
+
+
+-- Ejercicio 5: El nombre de los álbumes cuyas bandas fueron al menos a un concierto en el país "Argentina", ordenados por ranking de mejor a peor.
+
+
+
+-- Ejercicio 6: La cantidad de canciones que tiene cada banda, ordenadas de mayor a menor cantidad de canciones. Mostrar el nombre de la banda y la cantidad de canciones.
+
