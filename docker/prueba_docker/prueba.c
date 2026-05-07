c67940fa054006c9b1f439dc4f7a0b5e7e6240db
TValentinPerezG <tperezg@fi.uba.ar>
2026-05-07 01:33:07 +0000

    agregamos revision de los docentes

---
+++ docker/prueba_docker/prueba.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hola! Bienvenido\n");
+    printf("Hola! Bienvenido\n");
+    printf("Hola! Bienvenido\n");
+    
+    return 0;
+}
